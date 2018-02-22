fe432c96293cc85dd0f3d20d368292258a9946ce
mrzhangcc <cczhang1987@hotmail.com>
2018-02-21 23:08:08 -0500

    1 New files

---
+++ application/config/tables/menu.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created for jiuqian-desk.
+ * User: chuangchuangzhang
+ * Date: 2018/2/18
+ * Time: 16:24
+ *
+ * Desc:
+ */
+
+$config['tables/menu'] = array(
+    'm_id' => 'mid',
+    'm_name' => 'name',
+    'm_class' => 'class',
+    'm_parent' => 'parent',
+    'm_url' => 'url',
+    'm_displayorder' => 'displayorder',
+    'm_img' => 'img'
+);
